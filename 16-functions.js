293a079900bf91d92484156adda3968e14c2a3b7
9Justin9 <justinleon0412@gmail.com>
2026-08-12 22:48:57 +0200

    fein

---
+++ 16-functions.js
@@ -0,0 +1,19 @@
+// Functions
+function myFirstFunction() {
+    console.log(`Primera función mi gente, singamos.`);
+};
+
+for (let i = 0; i <= 5; i++) {
+    myFirstFunction();
+};
+
+function mySecondFunction (name) {
+    console.log (`Hola, ${name}, singamos.`);
+};
+
+let i = 0;
+while (i <= 3) {
+    mySecondFunction("nigger");
+    i++;
+};
+
