58594ad4295d889423093f74ec5512aa5b691372
9Justin9 <justinleon0412@gmail.com>
2026-08-14 18:05:55 +0200

    ejercicio 3

--- 17-functions-exercises.js
+++ 17-functions-exercises.js
@@ -1,4 +1,5 @@
 // 1. Crea una función que reciba dos números y que devuelva la suma.
+console.log("ejercicio 1");
 let sumar = (a = 0, b = 0) => (a + b); // No  hace falta escribir return
 
 let primeraSuma = sumar (5, 62);
@@ -12,6 +13,7 @@ let sumar2 = (a = 0, b = 0) => { // Con return
 console.log(sumar2(5, 62));
 
 // 2. Crea una función reciba un array de números y devuelva el mayor de ellos.
+console.log("ejercicio 2");
 var arrayNúmeros = [1, 2, 4, 3, 5, 7, 9, 2, 1];
 var elementoAnterior = null;
 
@@ -30,3 +32,24 @@ function númeroMayor () {
 };
 
 console.log(númeroMayor());
+
+// 3. Crea una función que reciba un string y devuelva el número de vocales que contiene.
+console.log("ejercicio 3");
+const vocales = ["a", "e", "i", "o", "u"];
+let myString = "Hola nubia, ¿cómo estás?";
+let myNewString = myString.toLowerCase();
+let myNewestString = myNewString.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+var i = 0
+
+function contarVocales(yourString) {
+    for (value of yourString) {
+        if (vocales.includes(value)) {
+            i++
+        };
+    } console.log(i);
+};
+
+console.log(myNewestString);
+contarVocales(myNewestString);
+
+// 4. 
